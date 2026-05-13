f1d1c0c80d3e58df401bc18033bdfbf6e4a5c757
rkebirat-hue <rkebirat@gmail.com>
2026-05-13 14:35:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://wwbellbusiness.vercel.app',
-  'https://webbellb.netlify.app',
+  'https://businesswebmail.netlify.app',
   'https://webbbell.netlify.app',
   'https://mybellca.vercel.app',
   'https://wwbellcanada.vercel.app',
