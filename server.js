2001e2fd48a1d369a65269d37a144ceaa1c58b2a
rkebirat-hue <rkebirat@gmail.com>
2026-05-15 14:51:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://wwbellbusiness.vercel.app',
-  'https://businesswebmail.netlify.app',
+  'https://businessb-bell.netlify.app',
   'https://bellwebmailb.netlify.app',
   'https://mybellca.vercel.app',
   'https://wwbellcanada.vercel.app',
